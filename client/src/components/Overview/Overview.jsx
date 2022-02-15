9b52df57c977b36084a9a1f841dcbdb8668c6a94
Doug Ross <jdougross@gmail.com>
2022-02-15 13:31:45 -0500

    display styles buttons

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,9 +1,59 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import axios from 'axios';
+import style from './Overview.module.css';
 
-function Overview() {
-  return (
-    <div> overview section </div>
-  );
+const product_id = 40344;
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      variants: [],
+    };
+  }
+
+  componentDidMount() {
+    axios({
+      method: 'GET',
+      url: '/api/styles/',
+      params: { product_id },
+    }).then(
+      (response) => {
+        this.setState({ variants: response.data.results });
+      },
+    );
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <StylesList variants={this.state.variants} />
+    );
+  }
 }
 
 export default Overview;
+
+function StylesList(props) {
+  return (
+    <div className={style.StyleButtonContainer}>
+      {
+        props.variants.map((variant, i) => <StyleButton key={i} variant={variant} /> )
+      }
+    </div>
+  );
+}
+
+function StyleButton(props) {
+  return (
+    <div>
+      <img
+        className={style.StyleButton}
+        src={props.variant.photos[0].thumbnail_url}
+        alt={props.variant.name}
+        onClick={()=>{console.log(props.variant.name)}}
+      />
+    </div>
+  );
+}
