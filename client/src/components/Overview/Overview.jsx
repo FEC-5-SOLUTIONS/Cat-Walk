f9d057bfc9e4bee303f1a6e9a6b66117db8ca1f7
Doug Ross <jdougross@gmail.com>
2022-02-15 14:03:35 -0500

    style images change main image

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 import axios from 'axios';
 import style from './Overview.module.css';
 
-const product_id = 40344;
+const product_id = 40346;
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       variants: [],
+      selectedVariant: {},
+      info: false,
     };
+
+    this.setSelectedVariant = this.setSelectedVariant.bind(this);
   }
 
   componentDidMount() {
@@ -22,37 +26,80 @@ class Overview extends React.Component {
       (response) => {
         this.setState({ variants: response.data.results });
       },
+    ).then( () => {
+      this.state.variants.forEach(
+        (v) => {
+          if (v['default?']) {
+            this.setState({ selectedVariant: v });
+          }
+        });
+      }
+    ).then( () => { this.setState({ info: true })}
     );
   }
 
+  setSelectedVariant(v) {
+    this.setState({ selectedVariant: v });
+  }
+
   render() {
-    console.log('render');
+    if (!this.state.info) { return <div> test </div>; }
+
     return (
-      <StylesList variants={this.state.variants} />
+      <div>
+        <FeaturedImage variant={this.state.selectedVariant} />
+        <StylesList variants={this.state.variants} setSelectedVariant={this.setSelectedVariant}/>
+      </div>
     );
   }
 }
 
 export default Overview;
 
+function FeaturedImage(props) {
+  if (!props.variant) { return <div>test</div> }
+  return (
+    <div>
+      <img
+        className={style.FeaturedImage}
+        src={props.variant.photos[0].url}
+        alt={props.variant.name}
+      />
+    </div>
+  );
+}
+
 function StylesList(props) {
+  function setSelectedVariant(v) {
+    props.setSelectedVariant(v);
+  }
   return (
     <div className={style.StyleButtonContainer}>
       {
-        props.variants.map((variant, i) => <StyleButton key={i} variant={variant} /> )
+        props.variants.map((variant, i) =>
+        <StyleButton
+          key={i}
+          variant={variant}
+          setSelectedVariant={setSelectedVariant}
+        /> )
       }
     </div>
   );
 }
 
 function StyleButton(props) {
+  function handleClick() {
+    console.log(props.variant.name);
+    props.setSelectedVariant(props.variant);
+  }
+
   return (
     <div>
       <img
         className={style.StyleButton}
         src={props.variant.photos[0].thumbnail_url}
         alt={props.variant.name}
-        onClick={()=>{console.log(props.variant.name)}}
+        onClick={handleClick}
       />
     </div>
   );
