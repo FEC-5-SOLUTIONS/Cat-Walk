3cf99639fba9e88475f106dde6dbe64851d9750b
Kevin Infante Medina <kevinfante@gmail.com>
2022-02-15 16:39:58 -0800

    Recommended percentage working.

--- client/src/components/Ratings_Reviews/StatsTab/Stats.jsx
+++ client/src/components/Ratings_Reviews/StatsTab/Stats.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import Stars from '../../Shared/Stars';
+import Recommended from './recommended';
 import styles from '../Ratings.module.css';
 
 function Stats({ metaData }) {
   return (
-      <div className={`${styles.statsTab} ${styles.statsFlex}`}>
-        <div className={styles.statsAvg}>
-          #
-        </div>
-        <div className={styles.statsStars}>
-          <Stars metaData={metaData} />
-        </div>
-        <div className={styles.statsRec}>
-          recomended
-        </div>
-        <div className={styles.statsBars}>
-          rating barss
-        </div>
-        <div className={styles.statsFit}>
-          fit
-        </div>
-        <div className={styles.statsFit}>
-          comfort
-        </div>
+    <div className={`${styles.statsTab} ${styles.statsFlex}`}>
+      <div className={styles.statsAvg}>
+        #
       </div>
+      <div className={styles.statsStars}>
+        <Stars metaData={metaData} />
+      </div>
+      <div className={styles.statsRec}>
+        <Recommended metaData={metaData} />
+      </div>
+      <div className={styles.statsBars}>
+        rating barss
+      </div>
+      <div className={styles.statsFit}>
+        fit
+      </div>
+      <div className={styles.statsFit}>
+        comfort
+      </div>
+    </div>
   );
 }
 
---
+++ client/src/components/Shared/Stars.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
+// import {getAvg} from '../utils/getAvg'
+// import { faStar } from '@fortawesome/free-regular-svg-icons';
+import styles from '../Ratings_Reviews/Ratings.module.css';
+
+const returnString = (begin, average) => {
+  if (average === 0) {
+    return 'No Reviews Yet';
+  }
+  let start = begin;
+  let returnJSX = [];
+  while (start < average) {
+    const diff = average - start;
+    if (diff >= 1) {
+      returnJSX.push(<FontAwesomeIcon icon={faStar} />);
+      start += 1;
+    } else if (diff > 0 && diff < 1) {
+      returnJSX.push(<FontAwesomeIcon icon={faStarHalfStroke} />);
+      start += 1;
+    }
+  }
+  return returnJSX;
+}
+
+const getAvg = (obj) => {
+  let totalCount = 0;
+  let totalSum = 0;
+  for (let i = 0; i < Object.values(obj).length; i += 1) {
+    totalCount += Number(Object.values(obj)[i]);
+    totalSum += ((Object.keys(obj)[i]) * Object.values(obj)[i]);
+  }
+  if ((totalSum / totalCount).isNaN) {
+    return 0;
+  }
+  return totalSum / totalCount;
+};
+
+function Stars({ metaData }) {
+  console.log('metaData: ', metaData);
+  let avg = 0;
+  // console.log('avg: ', avg);
+  let stars = {};
+  if (metaData.ratings) {
+    avg = getAvg(metaData.ratings);
+    stars = returnString(0, avg);
+  }
+
+  // !metaData.ratings ? null :
+
+  return !metaData.ratings ? null : (
+    <div>
+      <div>{avg}</div>
+      <div>
+        {stars.map((star) => star)}
+      </div>
+
+    </div>
+  );
+}
+
+export default Stars;
